8885f6fb9deddb984a97b5d49d287ff75c0bb449
hughestessa <147353819+hughestessa@users.noreply.github.com>
2023-12-11 02:40:12 +0000

    TP-Tessa-UpdatedInspoTab

--- teamprojectmobileapp/src/Inspiration/Inspiration.js
+++ teamprojectmobileapp/src/Inspiration/Inspiration.js
@@ -20,6 +20,14 @@ export default class Inspiration extends React.Component {
       <ScrollView style={styles.InspContainer}>
         <View >
             <Text style={styles.InspHeader}>Tips and Recipes</Text>
+            <Text></Text>
+            <Text style={styles.InspHeader2}>Tip of the Day:</Text>
+            <Text style={styles.InspBody}>Having trouble reaching your daily water goal? Try carrying a water bottle with you throughout the day!</Text>
+            <Text style={styles.InspHeader2}>Recipe of the Day:</Text>
+            <Text style={styles.InspBody}>Raspberry Lemonade Fizz:</Text>
+            <Text style={styles.InspBody}>Mash some raspberries in a cup. 
+            Add ice, a lemon slice, some sugar or stevia, and ice.
+            Let rest for a couple of minutes, then enjoy!</Text>
         </View>
       </ScrollView>
     )
@@ -30,9 +38,24 @@ const styles = StyleSheet.create({
   InspContainer: {
     backgroundColor: colors.primary,
     flex: 1,
+    
   },
   InspHeader: {
+    fontSize: 30,
+    color: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  InspHeader2: {
+    fontSize: 25,
+    color: 'black',
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginLeft: 10,
+  },
+  InspBody: {
+    marginLeft: 10,
     fontSize: 20,
-    color: 'white'
   }
 })
